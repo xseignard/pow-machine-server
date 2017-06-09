5983f6d0e437548bbd8ac95ce67326365ede4329
xseignard <xavier.seignard@gmail.com>
2017-06-09 11:15:35 +0200

    Added a button

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,10 @@
 (() => {
-	var socket = io.connect('http://207.154.232.181:4000');
-	var shakeEvent = new Shake({ threshold: 15 });
+	const socket = io.connect('http://207.154.232.181:4000');
+	const send = document.querySelector('#send');
+	send.onclick = () => {
+		socket.emit('shake');
+	};
+	const shakeEvent = new Shake({ threshold: 15 });
 	shakeEvent.start();
 	window.addEventListener(
 		'shake',
